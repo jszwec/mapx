fd311781f3befa4618888b907a443bf516962b84
Jacek Szwec <szwec.jacek@gmail.com>
2022-12-31 16:24:59 -0500

    perf: improve decoder performance for types that need to be converted

--- decoder.go
+++ decoder.go
@@ -71,19 +71,32 @@ func (dec *Decoder) decode(m map[string]any, dst reflect.Value) error {
 		}
 
 		switch {
-		case typ == f.typ:
+		case typ == f.typ || fastCanConvert(f.typ, typ):
 			switch typ.Kind() {
 			case reflect.String:
 				fv.SetString(val.String())
 			case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
 				fv.SetInt(val.Int())
+			case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
+				fv.SetUint(val.Uint())
 			case reflect.Float64, reflect.Float32:
 				fv.SetFloat(val.Float())
 			default:
 				fv.Set(val)
 			}
 		case val.CanConvert(f.typ):
-			fv.Set(val.Convert(f.typ))
+			switch f.typ.Kind() {
+			case reflect.String:
+				fv.SetString(val.Convert(f.typ).String())
+			case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+				fv.SetInt(val.Convert(f.typ).Int())
+			case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
+				fv.SetUint(val.Convert(f.typ).Uint())
+			case reflect.Float64, reflect.Float32:
+				fv.SetFloat(val.Convert(f.typ).Float())
+			default:
+				fv.Set(val.Convert(f.typ))
+			}
 		case f.typ.Kind() == reflect.Slice && val.Type().Kind() == reflect.Slice:
 			l := val.Len()
 			slice := reflect.MakeSlice(f.typ, l, l)
@@ -134,4 +147,27 @@ func (dec *Decoder) decode(m map[string]any, dst reflect.Value) error {
 
 func Decode(m map[string]any, v any, opts ...DecoderOpt) error {
 	return NewDecoder(opts...).Decode(m, v)
+}
+
+func fastCanConvert(typ, dst reflect.Type) bool {
+	switch typ.Kind() {
+	case reflect.String:
+		return dst.Kind() == reflect.String
+	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+		switch dst.Kind() {
+		case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+			return true
+		}
+	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
+		switch dst.Kind() {
+		case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
+			return true
+		}
+	case reflect.Float32, reflect.Float64:
+		switch dst.Kind() {
+		case reflect.Float32, reflect.Float64:
+			return true
+		}
+	}
+	return false
 }
